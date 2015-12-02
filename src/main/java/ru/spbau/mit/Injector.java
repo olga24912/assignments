8e7fad01d25dde5576411132c69229330781868d
Olga Chernikova <Oche24912@yandex.ru>
2015-12-02 17:52:44 +0300

    first Version

--- src/main/java/ru/spbau/mit/Injector.java
+++ src/main/java/ru/spbau/mit/Injector.java
@@ -1,5 +1,11 @@
 package ru.spbau.mit;
 
+import java.lang.reflect.AnnotatedType;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Parameter;
+import java.lang.reflect.TypeVariable;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 
@@ -9,7 +15,61 @@ public class Injector {
      * Create and initialize object of `rootClassName` class using classes from
      * `implementationClassNames` for concrete dependencies.
      */
+
+    private static ArrayList<Boolean> used = new ArrayList<>();
+
+    private static void resetUsed(int len) {
+        used.clear();
+        for (int i = 0; i < len; ++i) {
+            used.add(false);
+        }
+    }
+
+    private static Object myInitialize(String rootClassName, List<String> implementationClassNames) throws Exception {
+        if (used == null) {
+            used = new ArrayList<>();
+            for (int i = 0; i < (int) implementationClassNames.size(); ++i) {
+                used.add(false);
+            }
+        }
+        Class<?> rootClass = Class.forName(rootClassName);
+        Constructor constructorForRootClass = rootClass.getDeclaredConstructors()[0];
+        Class<?>[] parametersForRootClass = constructorForRootClass.getParameterTypes();
+        ArrayList<Object> myParameters = new ArrayList<>();
+        for (int j = 0; j < parametersForRootClass.length; ++j) {
+            Boolean flag = false;
+            for (int i = 0; i < implementationClassNames.size(); i++) {
+                Class<?> currentClass = Class.forName(implementationClassNames.get(i));
+                Class<?>[] interfacesForThisClass = currentClass.getInterfaces();
+                Boolean isGoodClass = currentClass.equals(parametersForRootClass[j]);
+                for (int g = 0; g < interfacesForThisClass.length; ++g) {
+                    if (interfacesForThisClass[g].equals(parametersForRootClass[j])) {
+                        isGoodClass = true;
+                    }
+                }
+                if (isGoodClass) {
+                    if (used.get(i)) {
+                        throw new InjectionCycleException();
+                    }
+                    if (flag) {
+                        throw new AmbiguousImplementationException();
+                    }
+                    used.set(i, true);
+                    Object goodClass = myInitialize(currentClass.getName(), implementationClassNames);
+                    myParameters.add(goodClass);
+                    flag = true;
+                }
+            }
+            if (!flag) {
+                throw new ImplementationNotFoundException();
+            }
+        }
+
+        return constructorForRootClass.newInstance(myParameters.toArray());
+    }
+
     public static Object initialize(String rootClassName, List<String> implementationClassNames) throws Exception {
-        throw new IllegalStateException();
+        resetUsed(implementationClassNames.size());
+        return myInitialize(rootClassName, implementationClassNames);
     }
 }
